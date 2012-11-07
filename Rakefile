e5ba58d4b244a5b2ac7290bb83e69239c4ecd4cb
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-07 13:16:12 +0800

    another try

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@ Bundler.setup
 Bundler.require
 
 Motion::Project::App.setup do |app|
-  Dir.glob(File.join(File.dirname(__FILE__), 'motion_i18n/*.rb')).each do |file|
+  Dir.glob(File.join(File.dirname(__FILE__), 'lib/**/*.rb')).each do |file|
     app.files.unshift(file)
   end
 end
--- lib/i18n.rb
+++ lib/i18n.rb
@@ -2,14 +2,13 @@ require "motion_i18n/version"
 require "motion_i18n/locale"
 
 module I18n
-  module Locale
-    def self.set(locale)
-      NSUserDefaults.standardUserDefaults.setObject([locale], forKey: "AppleLanguages")
-      NSUserDefaults.standardUserDefaults.synchronize
-    end
+  def self.set_locale(locale)
+    NSUserDefaults.standardUserDefaults.setObject([locale], forKey: "AppleLanguages")
+    NSUserDefaults.standardUserDefaults.synchronize
+  end
   
-    def self.current
-      NSLocale.preferredLanguages.objectAtIndex(0)
-    end
+  def self.current_locale
+    NSLocale.preferredLanguages.objectAtIndex(0)
   end
 end
+
