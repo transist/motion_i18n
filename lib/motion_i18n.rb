7605b97375b8ff55731ff2b02db37059e6b00f9b
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-07 13:11:03 +0800

    renaming

--- lib/motion_i18n.rb
+++ lib/motion_i18n.rb
@@ -1,2 +1,15 @@
 require "motion_i18n/version"
-require "motion_i18n/i18n"
+require "motion_i18n/locale"
+
+module I18n
+  module Locale
+    def self.set(locale)
+      NSUserDefaults.standardUserDefaults.setObject([locale], forKey: "AppleLanguages")
+      NSUserDefaults.standardUserDefaults.synchronize
+    end
+  
+    def self.current
+      NSLocale.preferredLanguages.objectAtIndex(0)
+    end
+  end
+end
