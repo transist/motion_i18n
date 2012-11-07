7dcb0f11d6732c7b9bdd68c8fad18e9a7a133864
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-07 13:39:53 +0800

    removing comments

--- lib/i18n.rb
+++ lib/i18n.rb
@@ -1,10 +1,3 @@
 module I18n
-  # def self.set_locale(locale)
-  #   NSUserDefaults.standardUserDefaults.setObject([locale], forKey: "AppleLanguages")
-  #   NSUserDefaults.standardUserDefaults.synchronize
-  # end
-  # 
-  # def self.current_locale
-  #   NSLocale.preferredLanguages.objectAtIndex(0)
-  # end
+
 end
